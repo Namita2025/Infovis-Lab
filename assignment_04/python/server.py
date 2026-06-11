1a373d9f635cc74df78f412e747f0836db0e15f7
Namita2025 <st192675@stud.uni-stuttgart.de>
2026-06-11 11:04:48 +0200

    Namita Birari_Assignment_04

--- assignment_04/python/server.py
+++ assignment_04/python/server.py
@@ -9,7 +9,27 @@ app.config["DEBUG"] = True
 def get_data():
     connection = sqlite3.connect("./data.db")
     df = pd.read_sql('''
-            SELECT * FROM iris
+            SELECT * FROM wine
+        ''', con=connection)
+    connection.close()
+    return Response(
+        response=df.to_json(orient="records"),
+        headers={"Access-Control-Allow-Origin": "*"}
+    )
+
+app.run(port=8080)
+from flask import Flask, Response, jsonify
+import pandas as pd
+import sqlite3
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/api/v1/get_data", methods=["GET"])
+def get_data():
+    connection = sqlite3.connect("./data.db")
+    df = pd.read_sql('''
+            SELECT * FROM wine
         ''', con=connection)
     connection.close()
     return Response(
